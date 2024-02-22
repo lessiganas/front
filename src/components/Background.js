2c6bfd5667c3852d94230425703657d955e18446
anaslessig <anaslessig>
2024-02-22 19:40:55 +0100

    first

--- src/components/Background.js
+++ src/components/Background.js
@@ -6,7 +6,7 @@ const Background = () => {
         <div className="top-site">
       <div className="background-img">
         {/* You can adjust the image path and other properties as needed */}
-        <img src="https://img.freepik.com/photos-gratuite/mise-plat-du-style-femme-accessoires-pull-tricot-rouge-chemise-carreaux-jeans-denim-bottes-cuir-noir-chapeau-tendance-mode-automne-vue-haut-appareil-photo-vintage-tenue-voyageur_285396-5116.jpg?w=996&t=st=1708393012~exp=1708393612~hmac=e4f23bc204f1bdaf324de1b6665bffd41a6ab2b72f5e558f23848d41dc57ee1b" alt="Background" />
+        <img src="" alt="Background" />
       </div>
       <div className="content">
         {/* Your content goes here */}
